603d46e58b2c210a73126570baba6881482b0bb8
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-06 13:27:22 +0000

    Create prescriptionAction.js file

---
+++ redux/prescriptions/prescriptionActions.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { doc, setDoc, collection, getDocs,onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase/firebase.js';
+
+const AddPrescription = createAsyncThunk(
+  'addPrescription',
+  async (
+    { id, customerId, pharmacistId, prescription, date, patientInfo, customer },
+    { rejectWithValue }
+  ) => {
+    try {
+      await setDoc(doc(db, 'prescriptions', id), {
+        id,
+        customerId,
+        customer,
+        pharmacistId,
+        patientInfo,
+        prescription,
+        date,
+      });
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  }
+);
+
+const GetAllPrescription = createAsyncThunk(
+  'getPrescription',
+  async (_, thunkAPI) => {
+    try {
+      const prescriptions = [];
+      const querySnapshot = await getDocs(collection(db, "prescriptions"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+        prescriptions.push(doc.data());
+      });
+      return prescriptions;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+/*
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  const cities = [];
+  querySnapshot.forEach((doc) => {
+      cities.push(doc.data().name);
+  });
+
+*/
+
+export { AddPrescription, GetAllPrescription };
