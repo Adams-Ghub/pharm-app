6c6b74cf1ab42e438a902dd5182b83606aa9d4c9
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-10 20:48:52 +0000

    modify prescriptionSlice

--- redux/prescriptions/prescriptionSlice.js
+++ redux/prescriptions/prescriptionSlice.js
@@ -1,41 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit';
-import{ AddPrescription,GetAllPrescription} from './prescriptionActions'; 
+import { AddPrescription, GetAllPrescription } from './prescriptionActions';
 
 const initialState = {
   prescriptions: [],
-  loading:false,
-  message:null,
-  error:null
+  loading: false,
+  message: null,
+  error: null,
 };
 
-const prescriptionSlice = createSlice(
-  {
-    name: 'prescription',
-    initialState,
-    reducers: {
-    },
-    extraReducers: (builder) => {
-      builder
-        .addCase(AddPrescription.fulfilled, (state, action) => {
-          state.currentUser = action.payload;
-          state.message = 'prescription successfully added'
-        })
-        .addCase(AddPrescription.rejected, (state, action) => {
-          state.error = action.payload;
-        })
-        .addCase(GetAllPrescription.pending, (state, action) => {
-          state.loading=true
-        })
-        .addCase(GetAllPrescription.fulfilled, (state, action) => {
-          state.prescriptions = action.payload;
-          state.loading=false
-        })
-        .addCase(GetAllPrescription.rejected, (state, action) => {
-          state.error = action.payload;
-        });
+const prescriptionSlice = createSlice({
+  name: 'prescription',
+  initialState,
+  reducers: {
+    getMyPrescriptions: (state, action) => {
+      const filtered = state.prescriptions.filter(
+        (prescription) => prescription.customerId === action.payload
+      );
+      return filtered
     },
   },
-);
+  extraReducers: (builder) => {
+    builder
+      .addCase(AddPrescription.fulfilled, (state, action) => {
+        state.currentUser = action.payload;
+        state.message = 'prescription successfully added';
+      })
+      .addCase(AddPrescription.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(GetAllPrescription.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(GetAllPrescription.fulfilled, (state, action) => {
+        state.prescriptions = action.payload;
+        state.loading = false;
+      })
+      .addCase(GetAllPrescription.rejected, (state, action) => {
+        state.error = action.payload;
+      });
+  },
+});
 
-export const { checkStatus } = prescriptionSlice.actions;
+export const { checkStatus,getMyPrescriptions } = prescriptionSlice.actions;
 export default prescriptionSlice.reducer;
