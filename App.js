6f7f06d8338823e987bea66b2d5970321642bf63
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-30 08:54:22 +0000

    Add persistor packate and alarm sound

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StyleSheet, View } from 'react-native';
 import MainNavigation from './src/navigation/main-navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store,persistor} from './redux/store';
 
 
 
@@ -10,9 +11,11 @@ export default function App() {
   return (
    
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer style={styles.container}>
           <MainNavigation />
         </NavigationContainer>
+        </PersistGate>
       </Provider>
     
   );
