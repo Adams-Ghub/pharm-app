984b161f19722aa5d5d05141d337fa7d8332a165
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-30 08:42:09 +0000

    Modify main navigation to allow login and logout features

--- src/navigation/main-navigation.js
+++ src/navigation/main-navigation.js
@@ -4,23 +4,42 @@ import Login from '../screens/login';
 import Signup from '../screens/signup';
 import PharmacistNavigation from './pharmacist-navigation';
 import ClientNavigation from './client-navigation';
+import { useSelector } from 'react-redux';
 
 function MainNavigation() {
   const Stack = createNativeStackNavigator();
+  const { user, loggedIn } = useSelector((state) => state.users);
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} options={{ header: () => {} }}  />
-      <Stack.Screen name="Signup" component={Signup} options={{ header: () => {} }} />
-      <Stack.Screen
-        name="PharmacistWelcome"
-        component={PharmacistNavigation}
-        options={{ header: () => {} }}
-      />
-      <Stack.Screen
-        name="ClientWelcome"
-        component={ClientNavigation}
-        options={{ header: () => {} }}
-      />
+      {loggedIn ? (
+        user.role === 'pharmacist' ? (
+          <Stack.Screen
+            name="PharmacistWelcome"
+            component={PharmacistNavigation}
+            options={{ header: () => {} }}
+          />
+        ) : (
+          <Stack.Screen
+            name="ClientWelcome"
+            component={ClientNavigation}
+            options={{ header: () => {} }}
+          />
+        )
+      ) : (
+        <>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ header: () => {} }}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ header: () => {} }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
