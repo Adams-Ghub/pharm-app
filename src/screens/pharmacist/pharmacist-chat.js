90efe8a0fc7747f416ebf23122db4e8206eab9e6
Adams-Ghub <bamfoadamsf@gmail.com>
2023-10-26 16:05:08 +0000

    Modify pharmasist chat screen

--- src/screens/pharmacist/pharmacist-chat.js
+++ src/screens/pharmacist/pharmacist-chat.js
@@ -1,78 +1,145 @@
-import React, { useCallback, useState, useLayoutEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, {
+  useCallback,
+  useState,
+  useLayoutEffect,
+  useEffect,
+} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  BackHandler,
+} from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { auth, db } from '../../../firebase/firebase';
 import { signOut } from 'firebase/auth';
-import { collection, addDoc, getDocs, query, orderBy, onSnapshot } from 'firebase/firestore';
+import {
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  onSnapshot,
+} from 'firebase/firestore';
+import { useSelector } from 'react-redux';
+import { useRoute } from '@react-navigation/native';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 const PharmacistChat = ({ navigation }) => {
-    const [messages, setMessages] = useState([]);
-    const signOutNow = () => {
-        signOut(auth).then(() => {
-            // Sign-out successful.
-            navigation.replace('Login');
-        }).catch((error) => {
-            // An error happened.
-        });
-    }
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerLeft: () => (
-                <View style={{ marginLeft: 20 }}>
-                    <Avatar
-                        rounded
-                        source={{
-                            uri: auth?.currentUser?.photoURL,
-                        }}
-                    />
-                </View>
-            ),
-            headerRight: () => (
-                <TouchableOpacity style={{
-                    marginRight: 10
-                }}
-                    onPress={signOutNow}
-                >
-                    <Text>logout</Text>
-                </TouchableOpacity>
-            )
-        })
-
-        const q = query(collection(db, 'chats'), orderBy('createdAt', 'desc'));
-        const unsubscribe = onSnapshot(q, (snapshot) => setMessages(
-            snapshot.docs.map(doc => ({
-                _id: doc.data()._id,
-                createdAt: doc.data().createdAt.toDate(),
-                text: doc.data().text,
-                user: doc.data().user,
-            }))
-        ));
-
-        return () => {
-          unsubscribe();
-        };
-
-    }, [navigation]);
-
-    const onSend = useCallback((messages = []) => {
-        const { _id, createdAt, text, user,to} = messages[0]
-
-        addDoc(collection(db, 'chats'), { _id, createdAt,  text, user,to });
-    }, []);
-
-    return (
-        <GiftedChat
-            messages={messages}
-            showAvatarForEveryMessage={true}
-            onSend={messages => onSend(messages)}
-            user={{
-                _id: auth?.currentUser?.email,
-                name: auth?.currentUser?.displayName,
-                avatar: auth?.currentUser?.photoURL
+  const [messages, setMessages] = useState([]);
+  const route = useRoute();
+  //   const navigation = useNavigation();
+  const signOutNow = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigation.replace('Login');
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <View style={{ marginTop: 80 }}>
+          <Avatar
+            rounded
+            source={{
+              uri: user.photo || 'image',
             }}
-        />
-    );
-}
+          />
+        </View>
+      ),
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginTop: 80,
+          }}
+          onPress={signOutNow}
+        >
+          <Text>logout</Text>
+        </TouchableOpacity>
+      ),
+    });
+
+    // useEffect(() => {
+    //   const backButtonHandler = () => {
+    //     // Check your condition here, for example:
+    //     if (shouldNavigateToSpecificScreen) {
+    //       navigation.navigate('Feedback'); // Navigate to your specific screen
+    //       return true; // Prevent default back behavior
+    //     }
+    //     return false; // Allow default back behavior
+    //   };
+
+    //   // Add the back button listener when the screen is focused
+    //   const backHandler = BackHandler.addEventListener(
+    //     'hardwareBackPress',
+    //     backButtonHandler
+    //   );
+
+    //   return () => {
+    //     // Remove the back button listener when the screen is unfocused
+    //     backHandler.remove();
+    //   };
+    // }, [navigation]);
+
+    const messagesRef = collection(db, 'chats', chatRoomId, 'messages');
+    const q = query(messagesRef, orderBy('createdAt', 'desc'));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setMessages(
+        snapshot.docs.map((doc) => ({
+          _id: doc.id, // Use doc.id as the message ID
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+        }))
+      );
+    });
+
+    // Ensure that you have access to the chat room ID when entering the chat room.
+
+    return () => {
+      unsubscribe();
+      console.log('good');
+      // <Text>Good</Text>;
+    };
+  }, [navigation]);
+
+  const to = route.params.item;
+  console.log('to:', to);
+  const { user } = useSelector((state) => state.users);
+
+  const chatRoomId = to.id.slice(0, 8) + user.id.slice(0, 8);
+
+  const onSend = useCallback((messages = []) => {
+    const chatRoomId = to.id.slice(0, 8) + user.id.slice(0, 8);
+
+    messages.forEach((message) => {
+      // Use addDoc to add the message to the "messages" subcollection of the chat room
+      addDoc(collection(db, 'chats', chatRoomId, 'messages'), {
+        createdAt: message.createdAt,
+        text: message.text,
+        user: message.user,
+      });
+    });
+  }, []);
+
+  return (
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={true}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: user.id,
+        name: user.name,
+        avatar: auth?.currentUser?.photoURL,
+      }}
+    />
+  );
+};
 
 export default PharmacistChat;
